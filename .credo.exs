7a0e4c8376240641b24cc71d53e00f3b3a78171f
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-04-13 16:38:47 -0400

    Remove additional config from Credo

--- .credo.exs
+++ .credo.exs
@@ -4,16 +4,7 @@
       name: "default",
       files: %{
         included: ["lib/", "test/"],
-      },
-      plugins: [],
-      requires: [],
-      strict: false,
-      parse_timeout: 5000,
-      color: true,
-      checks: [
-        {Credo.Check.Design.AliasUsage, priority: :low},
-        # ... other checks omitted for readability ...
-      ]
+      }
     }
   ]
 }
