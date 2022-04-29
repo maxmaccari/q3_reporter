36c6235e880b8f3c202788b65642d2f3eb53b3fc
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-04-29 19:49:35 -0400

    Ensure all application started

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,6 @@
 Code.require_file("support/log_helpers.exs", __DIR__)
 Q3Reporter.Log.ETSAdapter.init()
 
-Application.ensure_started(:q3_reporter)
+{:ok, _} = Application.ensure_all_started(:q3_reporter)
 
 ExUnit.start()
