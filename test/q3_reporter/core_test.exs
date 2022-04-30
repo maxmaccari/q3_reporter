ae1b06fa67a6be0c351cf478cea601dfe108d960
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-04-30 19:30:22 -0400

    Add should to Core tests

--- test/q3_reporter/core_test.exs
+++ test/q3_reporter/core_test.exs
@@ -8,11 +8,11 @@ defmodule Q3Reporter.CoreTest do
   describe "Core.log_to_results/2" do
     @content " 12:00 InitGame:\n 12:00 ClientConnect: 2\n 12:00 ShutdownGame:"
 
-    test "with default mode" do
+    test "should get Results with :by_game mode" do
       assert %Results{entries: [%{}], mode: :by_game} = Core.log_to_results(@content, :by_game)
     end
 
-    test "with ranking mode" do
+    test "should get Results with :ranking mode" do
       assert %Results{entries: [%{}], mode: :ranking} = Core.log_to_results(@content, :ranking)
     end
   end
@@ -20,18 +20,18 @@ defmodule Q3Reporter.CoreTest do
   describe "Core.log_to_games/1" do
     @content " 12:00 InitGame:\n 12:00 ClientConnect: 2\n 12:00 ShutdownGame:"
 
-    test "convert log into a list of games" do
+    test "should convert log into a list of games" do
       assert [%Game{}] = Core.log_to_games(@content)
     end
   end
 
   describe "Core.games_to_results/2" do
-    test "convert log into a list of games with default mode" do
+    test "should convert log into a list of games with :by_game mode" do
       assert %Results{entries: [%{}], mode: :by_game} =
                Core.games_to_results([%Q3Reporter.Core.Game{}])
     end
 
-    test "convert log into a list of games with ranking mode" do
+    test "should convert log into a list of games with :ranking mode" do
       assert %Results{entries: [], mode: :ranking} =
                Core.games_to_results([%Q3Reporter.Core.Game{}], :ranking)
     end
