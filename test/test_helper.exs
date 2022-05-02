785ba83e8c8abaad542f8310f1527852601915fc
Maxsuel Maccari <maxmaccari@hotmail.com>
2022-05-02 12:39:15 -0400

    Add error adapters

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,5 @@
 Code.require_file("support/log_helpers.exs", __DIR__)
+Code.require_file("support/error_adapters.exs", __DIR__)
 Q3Reporter.Log.ETSAdapter.init()
 
 {:ok, _} = Application.ensure_all_started(:q3_reporter)
